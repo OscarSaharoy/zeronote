6c1461f19f12be9781391823393957399bc3b4a9
Oscar Saharoy <osaharoy@gmail.com>
2022-10-02 20:53:55 +0100

    add download upload

---
+++ js/download-upload.js
@@ -0,0 +1,36 @@
+// Oscar Saharoy 2022
+
+const svgCanvas = document.getElementById( "canvas" );
+
+document.addEventListener( "keydown", event => event.key == 'd' ? downloadSVG() : null );
+
+// svg downloading function
+function downloadSVG() {
+
+    // get svg data and make into a blob
+    const data = svgCanvas.outerHTML;
+    const blob = new Blob( [data], {type: 'image/svg+xml'} );
+
+    // create a dummy link with the blob
+    const elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = "zeronote.svg";
+    elem.click();
+}
+
+
+document.addEventListener( "keydown", event => event.key == 'u' ? uploadSVG() : null );
+
+// svg uploading function
+function uploadSVG() {
+
+    // get svg data and make into a blob
+    const data = svgCanvas.outerHTML;
+    const blob = new Blob( [data], {type: 'image/svg+xml'} );
+
+    // create a dummy link with the blob
+    const elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = "zeronote.svg";
+    elem.click();
+}
