557e5e66076989642f7da0c5b7d402313d9122d8
Oscar Saharoy <osaharoy@gmail.com>
2022-11-27 23:42:17 +0000

    unset erase on colour pick

--- js/colour-picker.js
+++ js/colour-picker.js
@@ -2,6 +2,7 @@
 
 const penTip = document.getElementById( "pen-tip" );
 const pickerInput = document.getElementById( "colour-picker-input" );
+const eraseButton = document.getElementById( "erase" );
 
 // variable that holds the current selected stroke colour
 export let currentColour = "#ffffff";
@@ -13,4 +14,5 @@ updatePenTip();
 // link input on colour picker to update ui
 pickerInput.addEventListener( "input", () => currentColour = pickerInput.value );
 pickerInput.addEventListener( "input", updatePenTip );
+pickerInput.addEventListener( "input", () => eraseButton.click() );
 
