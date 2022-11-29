e26af1cdb93fff4c6c77447f3a94a45c5d6b9e59
Oscar Saharoy <osaharoy@gmail.com>
2022-11-29 15:10:45 +0000

    only click erase button on colour choose if erase active

--- js/colour-picker.js
+++ js/colour-picker.js
@@ -1,8 +1,9 @@
 // Oscar Saharoy 2022
 
+import { eraseButtonClicked, eraseButton } from "./erase-button.js";
+
 const penTip = document.getElementById( "pen-tip" );
 const pickerInput = document.getElementById( "colour-picker-input" );
-const eraseButton = document.getElementById( "erase" );
 
 // variable that holds the current selected stroke colour
 export let currentColour = "#ffffff";
@@ -14,5 +15,5 @@ updatePenTip();
 // link input on colour picker to update ui
 pickerInput.addEventListener( "input", () => currentColour = pickerInput.value );
 pickerInput.addEventListener( "input", updatePenTip );
-pickerInput.addEventListener( "input", () => eraseButton.click() );
+pickerInput.addEventListener( "input", () => eraseButtonClicked ? eraseButton.click()  : null );
 
--- js/erase-button.js
+++ js/erase-button.js
@@ -1,7 +1,6 @@
 // Oscar Saharoy 2022
 
-const eraseButton = document.getElementById( "erase" );
-
+export const eraseButton = document.getElementById( "erase" );
 export let eraseButtonClicked = false;
 
 eraseButton.addEventListener( "click", () => 
