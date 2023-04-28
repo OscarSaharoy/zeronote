ebfab2e1831014d189029135aff066cb27168a39
Oscar Saharoy <osaharoy@gmail.com>
2023-04-28 10:47:31 +0100

    fix touch toolbox dragging bug and make button targets bigger

--- js/move-toolbox.js
+++ js/move-toolbox.js
@@ -6,6 +6,7 @@ const logo = document.getElementById( "logo" );
 
 let moving = false;
 let [ dx, dy ] = [0, 0];
+let [ x, y ] = [ null, null ];
 
 
 grip.addEventListener( "pointerdown", gripStart );
@@ -25,19 +26,22 @@ const clamp = (x, min, max) => x < min ? min : ( x > max ? max : x );
 function gripStart( event ) {
 	moving = true;
 	grip.setPointerCapture(event.pointerId);
+	[ x, y ] = [ event.clientX, event.clientY ];
 }
 
 function gripMove( event ) {
 
 	if( !moving ) return;
 
-	[ grip, logo ].forEach( elm => elm.style.cursor="grabbing" )
+	[ grip, logo ].forEach( elm => elm.style.cursor="grabbing" );
 
-	dx += event.movementX;
-	dy += event.movementY;
+	[ dx, dy ] = [ dx + event.clientX - x, dy + event.clientY - y ];
+	[ x, y ] = [ event.clientX, event.clientY ];
 
 	dx = clamp( dx, 0, window.innerWidth  - toolbox.clientWidth  );
 	dy = clamp( dy, 0, window.innerHeight - toolbox.clientHeight );
+
+	console.log( event.movementX, event.movementY );
 
 	toolbox.style.transform = `translateX(${dx}px) translateY(${dy}px)`;
 }
